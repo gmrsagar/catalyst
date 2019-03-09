b6ab41b03b2baae4f6539cef3e59f5a3de1304e6
Sagar Ghimire <gmrsagar@gmail.com>
2019-03-09 13:43:52 +1100

    Parse the given file

--- user_upload.php
+++ user_upload.php
@@ -3,41 +3,49 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Database\Database;
+use Helpers\Parser;
 
 //0th position is phpfile name
 //1st position is arguments , we can ignore other for fix it to 2
     $argument1 = $argv[1];
     if (substr( $argument1, 0, 2 ) === "--") {
     // It starts with '--'
-    $command = substr($argument1, 2);
+        $command = substr($argument1, 2);
 
-    switch ($command) {
-        case 'create_table':
-            //get db connection
-            $db = Database::getInstance()->getLink();
-            
-            $sql = 'CREATE TABLE `users` ( 
-                `id` INT NOT NULL AUTO_INCREMENT ,
-                `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-                `surname` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-                `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-                PRIMARY KEY (`id`),
-                UNIQUE `UNIQUE` (`email`(255)))
-                ENGINE = InnoDB';
-            $result = mysqli_query($db, $sql) or die(mysqli_error($db));
-            echo "Table created successfully";
-        break;
+        switch ($command) {
+            case 'create_table':
+                //get db connection
+                $db = Database::getInstance()->getLink();
+                
+                $sql = 'CREATE TABLE `users` ( 
+                    `id` INT NOT NULL AUTO_INCREMENT ,
+                    `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+                    `surname` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+                    `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+                    PRIMARY KEY (`id`),
+                    UNIQUE `UNIQUE` (`email`(255)))
+                    ENGINE = InnoDB';
+                $result = mysqli_query($db, $sql) or die(mysqli_error($db));
+                echo "Table created successfully";
+            break;
 
-        case 'help':
-            //print the list of commands
-        break;
+            case 'help':
+                //print the list of commands
+            break;
 
-        case 'file':
-            //file parsing code
-        break;
+            case 'file':
+                $filename = $argv[2];
+                $filename = $filename.'.csv';
+                
+                //file parsing code
+                $parser = Parser::getInstance();
+                $file = $parser->parseCsv($filename);
+                print_r($file);
+                die;
+            break;
 
-        default:
-            echo "i dont know  this";
+            default:
+                echo "i dont know  this";
         }
     }
     else {
